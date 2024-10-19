d659ab8025cf351a5478abd1a534a77c26cdb4ec
Naresh hans <nareshhans37@gmail.com>
2024-10-19 11:08:28 +0530

    adding the require files for project.

---
+++ project.js
@@ -0,0 +1,15 @@
+// 1. deposit money for the betting
+// 2. determine the number of lines to bet on
+// 3. Collect the bet amount
+// 4. spin the slot muchine
+// 5. check if the user win
+// 6. if win give the winning amount
+// 7. play again
+
+const prompt = requre('promt-sync')();
+
+const deposit = () =>{
+    const depositAmount = promt('Enter a deposit amount');
+}
+
+deposit();
