73b31dd00a78befb48adec7ed57466f7471cd6b9
Naresh hans <nareshhans37@gmail.com>
2024-10-19 11:46:09 +0530

    added function for deposit and number of lines to bet on

--- project.js
+++ project.js
@@ -6,10 +6,35 @@
 // 6. if win give the winning amount
 // 7. play again
 
-const prompt = requre('promt-sync')();
+const prompt = require('prompt-sync')();
 
 const deposit = () =>{
-    const depositAmount = promt('Enter a deposit amount');
+
+    while(true){
+        const depositAmount = prompt('Enter a deposit amount: ');
+        const numberDepositAmount = parseFloat(depositAmount)
+        if(isNaN(numberDepositAmount) || numberDepositAmount <=0){
+            console.log('Invalid deposit amount, try again');
+        }else{
+            return numberDepositAmount;
+        }
+    }
 }
 
-deposit();
+const numberOfLines = () =>{
+    while(true){
+        const Lines = prompt("Choose any number of lines to bet on from 1-3: ");
+        const numOfLInes = parseInt(Lines);
+        if(isNaN(numOfLInes) || numOfLInes == 0 || numOfLInes > 3){
+            console.log('Choose number from 1 to 3');
+        }else{
+            return numOfLInes;
+        }
+    }
+
+}
+
+const depositAmount = deposit();
+const numOfLInes = numberOfLines();
+console.log('Deposit amount: '+ depositAmount);
+console.log('Number of lines to bet on: '+ numOfLInes);
